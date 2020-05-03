767f0151af7fe44d27a07dfa342187993fab61da
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-05-03 19:57:21 +0100

    Add js files

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  var rootElem = document.getElementById("root");
+  //rootElem.textContent = `Got ${episodeList[0].id} episode(s)`;
+  for (var i = 0; i < episodeList.length; i++) {
+    var element = document.createElement("h1");
+    var image = document.createElement("img");
+    var text = document.createElement("p");
+
+    element.textContent = `${episodeList[i].name} - S0${episodeList[i].season} E0 ${episodeList[i].number}`;
+    image.src = episodeList[i].image.medium;
+    text.textContent = episodeList[i].summary;
+    rootElem.appendChild(element);
+    rootElem.appendChild(image);
+    rootElem.appendChild(text);
+  }
+  return rootElem;
 }
 
 window.onload = setup;
