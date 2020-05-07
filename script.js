dafba5a56a049d30b68893f8c66678434d0c0703
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-05-07 16:29:32 +0100

    Fix the lay out of the cards

--- script.js
+++ script.js
@@ -8,18 +8,35 @@ function makePageForEpisodes(episodeList) {
   var rootElem = document.getElementById("root");
   //rootElem.textContent = `Got ${episodeList[0].id} episode(s)`;
   for (var i = 0; i < episodeList.length; i++) {
-    var element = document.createElement("h1");
+    var container = document.createElement("div");
+    var element = document.createElement("h4");
     var image = document.createElement("img");
     var text = document.createElement("p");
 
-    element.textContent = `${episodeList[i].name} - S0${episodeList[i].season} E0 ${episodeList[i].number}`;
+    element.textContent = `${episodeList[i].name}-S0${episodeList[i].season}E0${episodeList[i].number}`;
     image.src = episodeList[i].image.medium;
-    text.textContent = episodeList[i].summary;
-    rootElem.appendChild(element);
-    rootElem.appendChild(image);
-    rootElem.appendChild(text);
+    text.innerHTML = `${
+      episodeList[i].summary.length < 50
+        ? episodeList[i].summary
+        : episodeList[i].summary.substring(0, 100)
+    }....`;
+
+    element.classList.add("title");
+    image.classList.add("image");
+    text.classList.add("text");
+    container.classList.add("container");
+
+    container.appendChild(element);
+    container.appendChild(image);
+    container.appendChild(text);
+    rootElem.appendChild(container);
   }
   return rootElem;
 }
+
+const episodeFormat = function(es) {
+  let number = Number(es);
+  return "£" + number.toFixed(2).toLocaleString() + " ";
+};
 
 window.onload = setup;
